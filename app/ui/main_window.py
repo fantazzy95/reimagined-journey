c94d802d2a51df3bf04771156dcc11de61c9951c
fantazzy95 <53875168+fantazzy95@users.noreply.github.com>
2026-04-24 10:15:51 +0300

    Upgrade main menu to MVP

--- app/ui/main_window.py
+++ app/ui/main_window.py
@@ -1,26 +1,63 @@
-from PySide6.QtWidgets import QMainWindow, QPushButton, QWidget, QVBoxLayout
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QPushButton
 
+from app.ui.product_window import ProductWindow
+from app.ui.import_window import ImportWindow
+from app.ui.receipt_window import ReceiptWindow
 from app.ui.pricing_window import PricingWindow
+from app.ui.stock_window import StockWindow
 
 
 class MainWindow(QMainWindow):
-    def __init__(self, inventory_service):
+    def __init__(self, db):
         super().__init__()
-        self.setWindowTitle("Inventory App")
-        self.resize(800, 600)
+        self.setWindowTitle("ERP Lite")
+        self.resize(400, 500)
+
+        self.db = db
 
         central = QWidget()
         layout = QVBoxLayout()
 
-        btn_pricing = QPushButton("Встановлення цін")
+        btn_products = QPushButton("Товари")
+        btn_products.clicked.connect(self.open_products)
+
+        btn_import = QPushButton("Імпорт")
+        btn_import.clicked.connect(self.open_import)
+
+        btn_receipt = QPushButton("Надходження")
+        btn_receipt.clicked.connect(self.open_receipt)
+
+        btn_pricing = QPushButton("Ціни")
         btn_pricing.clicked.connect(self.open_pricing)
 
-        layout.addWidget(btn_pricing)
-        central.setLayout(layout)
+        btn_stock = QPushButton("Склад")
+        btn_stock.clicked.connect(self.open_stock)
 
+        layout.addWidget(btn_products)
+        layout.addWidget(btn_import)
+        layout.addWidget(btn_receipt)
+        layout.addWidget(btn_pricing)
+        layout.addWidget(btn_stock)
+
+        central.setLayout(layout)
         self.setCentralWidget(central)
-        self.inventory_service = inventory_service
+
+    def open_products(self):
+        self.w = ProductWindow(self.db)
+        self.w.show()
+
+    def open_import(self):
+        self.w = ImportWindow(self.db)
+        self.w.show()
+
+    def open_receipt(self):
+        self.w = ReceiptWindow(self.db)
+        self.w.show()
 
     def open_pricing(self):
-        self.pricing_window = PricingWindow(self.inventory_service.db)
-        self.pricing_window.show()
+        self.w = PricingWindow(self.db)
+        self.w.show()
+
+    def open_stock(self):
+        self.w = StockWindow(self.db)
+        self.w.show()
